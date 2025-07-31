b6c705f8d7ed59a6a94920d1cc4f73539e76d502
Marina-Chuhlib <marinachuhlib@gmail.com>
2025-07-31 19:17:51 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-
   reactStrictMode: true,
-  images: {
-    domains: ['example.com'],
-  },
 };
 
 export default nextConfig;
